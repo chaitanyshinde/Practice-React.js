0b15479a282e3e18ab70c1ca78f6a25fe51a7d35
user.name "chaitanyshinde2323@gmail.com <chaitanyshinde2323@gmail.com>
2025-05-17 12:22:42 +0530

    usestate

--- firstApp/src/App.jsx
+++ firstApp/src/App.jsx
@@ -1,9 +1,27 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const incCount = () => {
+    setCount(count + 1);
+  };
+  const decCount = () => {
+    setCount(count - 1);
+  };
   return (
     <div>
       <h1>First Hello!</h1>
+      <h2>Count: {count}</h2>
+      <button onClick={incCount}>Increase Count</button>
+      <button onClick={decCount}>Decrease Count</button>
+      <button
+        onClick={() => {
+          setCount(count + 10);
+        }}
+      >
+        Inc by 10
+      </button>
     </div>
   );
 }
