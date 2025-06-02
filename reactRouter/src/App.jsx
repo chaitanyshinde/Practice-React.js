bf7865db21790bfbe020e671d6a913ff30e7f4b4
user.name "chaitanyshinde2323@gmail.com <chaitanyshinde2323@gmail.com>
2025-06-02 08:27:33 +0530

    new concept

---
+++ reactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="bg-green-600 flex items-center justify-center">
+        Hii Hello
+      </div>
+    </>
+  );
+}
+
+export default App;
