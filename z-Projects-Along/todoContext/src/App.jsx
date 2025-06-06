2c1546a1eb03d05919c8c2f4a8a87cec7cf8e1a7
user.name "chaitanyshinde2323@gmail.com <chaitanyshinde2323@gmail.com>
2025-06-06 23:39:34 +0530

    pro-practice

--- z-Projects-Along/todoContext/src/App.jsx
+++ z-Projects-Along/todoContext/src/App.jsx
@@ -1,10 +1,65 @@
+import { useEffect, useState } from "react";
+import { TodoProvider } from "./context/TodoContext";
+import TodoForm from "./components/TodoForm";
+import TodoItem from "./components/TodoItem";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prv) => [{ id: Date.now(), ...todo }, ...prv]);
+  };
+
+  const updateTodo = (id, todo) => {
+    setTodos((prvTodos) => prvTodos.map((prv) => (prv.id === id ? todo : prv)));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prv) => prv.filter((prvTodo) => prvTodo.id !== id));
+  };
+  const toggleComplete = (id) => {
+    setTodos((prv) =>
+      prv.map((prvTodo) =>
+        prvTodo.id === id
+          ? { ...prvTodo, completed: !prvTodo.completed }
+          : prvTodo
+      )
+    );
+  };
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <>
-      <h1 className="bg-amber-500">
-        Hello Todo
-      </h1>
-    </>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">
+            <TodoForm />
+          </div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id} className="w-full">
+                <TodoItem todo={todo} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </TodoProvider>
   );
 }
 
